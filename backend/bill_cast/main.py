ac4a6a6d7f3c0d91e3bc3673acda300b1b1141f7
Alexander <ninjaduckiduck@gmail.com>
2022-07-17 16:37:19 +0200

    Return cors restristions

--- backend/bill_cast/main.py
+++ backend/bill_cast/main.py
@@ -50,8 +50,8 @@ app.mount("/", SinglePageApplication(directory="../dist"), name="spa")
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins="*",
-        # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        # allow_origins="*",
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
